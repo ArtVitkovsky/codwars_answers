14f11e1e4951dc6e635b908d48c540da336770d7
Artem Vitkovsky <workwith9512@gmail.com>
2023-01-05 20:39:03 +0300

    feat: add new answers

---
+++ day2.py
@@ -0,0 +1,16 @@
+def make_upper_case(s):
+    return s.upper()
+
+
+# Given a year, return the century it is in.
+def century(year):
+    import math as m
+    cent = len(str(year))
+    if year in range(0, 100):
+        return 1
+    elif cent >= 3 and year % 100 != 0:
+        return m.floor((year / 100) + 1)
+    elif cent >= 3 and year % 100 == 0:
+        return int(year / 100)
+
+print(century(1250))
