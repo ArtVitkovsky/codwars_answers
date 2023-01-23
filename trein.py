1783216edb1ed44f3854bc8e8f397d10d3e7683a
Artem Vitkovsky <workwith9512@gmail.com>
2023-01-23 22:21:38 +0300

    feat: add new task

--- trein.py
+++ trein.py
@@ -1,6 +1,8 @@
-num = 58912365
-lst = []
-for i in str(num):
-    lst.append(int(i))
-sr = sorted(lst, reverse=True)
-print(sr)
+def remove_smallest(numbers):
+    new_tup = []
+    if numbers is list:
+        numbers.remove(min(numbers))
+        return numbers
+
+
+print(remove_smallest([2, 1, 8, 1, 9, 5]))
